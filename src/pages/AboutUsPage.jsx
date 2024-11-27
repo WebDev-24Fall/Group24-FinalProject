ec10b036fce471e3fa8f5421678c4f8069a79578
guyunxiang <guyunxiang32@gmail.com>
2024-11-27 09:29:53 -0500

    optimize About Us page, support mobile devices styles

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -7,12 +7,12 @@ const AboutUsPage = () => {
   return (
     <div className={styles.aboutUs}>
       <div className="container mx-auto p-6 relative z-10 bg-black/[.75]">
-        <h1 className="text-white text-center text-xl sm:text-2xl md:text-3xl mb-3">
+        <h1 className="text-white text-center text-xl sm:text-2xl md:text-4xl mb-3">
           Trend in Web Technology
         </h1>
-        <h2 className="mb-12 text-center text-xl sm:text-xl md:text-2xl">Final Project - Group 24</h2>
-        <div className="flex flex-row justify-center gap-3 px-3">
-          <div className="w-1/3">
+        <h2 className="mb-10 md:mb-20 text-center text-lg md:text-2xl">Final Project - Group 24</h2>
+        <div className="flex flex-col md:flex-row justify-center gap-3 px-3">
+          <div className="md:w-1/3">
             <img src={quote} alt="Quote Icon" className="w-10" />
             <h2 className="my-3 font-bold">Blockchain</h2>
             <p className="my-3">
@@ -22,7 +22,7 @@ const AboutUsPage = () => {
             </p>
             <p>- Gu, Yunxiang (8904492)</p>
           </div>
-          <div className="w-1/3">
+          <div className="md:w-1/3">
             <img src={quote} alt="Quote Icon" className="w-10" />
             <h2 className="my-3 font-bold">Lorem ipsum dolor</h2>
             <p className="my-3">
@@ -32,7 +32,7 @@ const AboutUsPage = () => {
             </p>
             <p>- Abdul Mannan, Mohammed</p>
           </div>
-          <div className="w-1/3">
+          <div className="md:w-1/3">
             <img src={quote} alt="Quote Icon" className="w-10" />
             <h2 className="my-3 font-bold">Lorem ipsum dolor</h2>
             <p className="my-3">
