f9227c54da010de4a0d58921dcf1ca03c8d051c7
guyunxiang <guyunxiang32@gmail.com>
2024-12-13 15:12:59 -0500

    add Contact Us route

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-  import React from "react";
-  import ReactDOM from "react-dom/client";
-  import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-  import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 
-  import "./index.css";
+import "./index.css";
 
-  import Layout from "./components/Layout";
-  import HomePage from "./pages/HomePage";
-  import AboutUsPage from "./pages/AboutUsPage";
-  import FlavourPage from "./pages/FlavourPage";
-  import FlavourPage2 from "./pages/FlavourPage2";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import AboutUsPage from "./pages/AboutUsPage";
+import FlavourPage from "./pages/FlavourPage";
+import FlavourPage2 from "./pages/FlavourPage2";
+import ContactUsPage from "./pages/ContactUsPage"; 
 
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-    <React.StrictMode>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about-us" element={<AboutUsPage />} />
-            <Route path="*" element={<HomePage />} />
-            <Route path="/flavour" element={<FlavourPage />} />
-            <Route path="/flavour2" element={<FlavourPage2 />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </React.StrictMode>
-  );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="*" element={<HomePage />} />
+          <Route path="/flavour" element={<FlavourPage />} />
+          <Route path="/flavour2" element={<FlavourPage2 />} />
+          <Route path="/contactus" element={<ContactUsPage />} /> 
+        </Routes>
+      </Layout>
+    </Router>
+  </React.StrictMode>
+);
 
-  reportWebVitals();
+reportWebVitals();
